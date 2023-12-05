222bef2892210aa374ee1311d4581c79d36cf1c5
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2023-12-05 02:48:05 +0000

    Images and text now showing when tapping buttons

--- mealsapp/lib/screens/categories.dart
+++ mealsapp/lib/screens/categories.dart
@@ -2,12 +2,25 @@ import 'package:flutter/material.dart';
 
 import 'package:mealsapp/data/dummy_data.dart';
 import 'package:mealsapp/widgets/category_grid_item.dart';
+import 'package:mealsapp/screens/meals.dart';
+import 'package:mealsapp/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory() {
-    Navigator.push(context, route);
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
+        ),
+      ),
+    ); // Navigator.push(context, route)
   }
 
   @override
@@ -27,7 +40,12 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            )
         ],
       ),
     );
