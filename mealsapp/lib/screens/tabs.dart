64bd3bad812f9d5b43d19f065b75959cee596b2f
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2023-12-09 23:00:27 +0000

    Connecting multiple providers

--- mealsapp/lib/screens/tabs.dart
+++ mealsapp/lib/screens/tabs.dart
@@ -5,7 +5,6 @@ import 'package:mealsapp/screens/categories.dart';
 import 'package:mealsapp/screens/filters.dart';
 import 'package:mealsapp/screens/meals.dart';
 import 'package:mealsapp/widgets/main_drawer.dart';
-import 'package:mealsapp/providers/meals_provider.dart';
 import 'package:mealsapp/providers/favorites_provider.dart';
 import 'package:mealsapp/providers/filters_provider.dart';
 
@@ -48,25 +47,7 @@ class _TabsScreenState extends ConsumerState<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final meals = ref.watch(mealsProvider);
-    final activeFilters = ref.watch(filtersProvider);
-    final availableMeals = meals.where(
-      (meal) {
-        if (activeFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-          return false;
-        }
-        if (activeFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-          return false;
-        }
-        if (activeFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-          return false;
-        }
-        if (activeFilters[Filter.vegan]! && !meal.isVegan) {
-          return false;
-        }
-        return true;
-      },
-    ).toList();
+    final availableMeals = ref.read(filteredMealsProvider);
 
     Widget activePage = CategoriesScreen(
       availableMeals: availableMeals,
