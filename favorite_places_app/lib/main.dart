2507b497e2337332aa79873d07232728a50a28e9
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2024-01-03 14:02:34 +0000

    Fixed

--- favorite_places_app/lib/main.dart
+++ favorite_places_app/lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter/material.dart';
 
-import 'package:favorite_places_app/screens/places.dart';
+import 'package:favorite_places/screens/places.dart';
 
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
@@ -29,14 +29,12 @@ final theme = ThemeData().copyWith(
 
 void main() {
   runApp(
-    const ProviderScope(
-      child: MyApp(),
-    ),
+    const ProviderScope(child: MyApp()),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
