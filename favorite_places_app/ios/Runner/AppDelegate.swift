316d497cfc7a02d179bed5053d8df087078578f4
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2024-01-07 20:37:55 +0000

    Adding a Map Screen

--- favorite_places_app/ios/Runner/AppDelegate.swift
+++ favorite_places_app/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyC3D6_pgC1h41Qo4_B775d2kXuXHqiZATg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
