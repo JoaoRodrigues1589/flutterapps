03d928f4ed40d011876310c1115fc423594f985c
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2024-01-07 19:25:08 +0000

    Using the Google Maps API

--- favorite_places_app/lib/widgets/location_input.dart
+++ favorite_places_app/lib/widgets/location_input.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -42,13 +45,19 @@ class _LocationInputState extends State<LocationInput> {
     });
 
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+
+    final url = Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=AIzaSyC3D6_pgC1h41Qo4_B775d2kXuXHqiZATg');
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
 
     setState(() {
       _isGettingLocation = false;
     });
 
-    print(locationData.latitude);
-    print(locationData.longitude);
+
   }
 
   @override
