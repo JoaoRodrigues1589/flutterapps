47419311bee830ec5bcbb2a5b39edf6217b47e7b
JoaoRodrigues1589 <joaorodrigues1589@gmail.com>
2024-01-10 19:20:33 +0000

    Adding Buttons

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -10,6 +10,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var _isLogin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,10 +32,10 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Image.asset('assets/images/chat.png'),
               ),
               Card(
-                margin: EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
                 child: SingleChildScrollView(
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: const EdgeInsets.all(16),
                     child: Form(
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
@@ -52,6 +54,26 @@ class _AuthScreenState extends State<AuthScreen> {
                             ),
                             obscureText: true,
                           ),
+                          const SizedBox(height: 12),
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer,
+                            ),
+                            child: Text(_isLogin ? 'Login' : 'Signup'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              setState(() {
+                                _isLogin = !_isLogin;
+                              });
+                            },
+                            child: Text(_isLogin
+                                ? 'Create an account'
+                                : 'I already have an account.'),
+                          )
                         ],
                       ),
                     ),
